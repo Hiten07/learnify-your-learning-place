083f990e0eae36133f74f473bd6893714fc9508a
hitenthanki <hiten.thanki@esparkbizmail.com>
2025-08-05 14:51:15 +0530

    inital commit

--- frontend/src/utils/Loader.tsx
+++ frontend/src/utils/Loader.tsx
@@ -2,5 +2,4 @@ export const Loader = () => (
     <div className="fixed inset-0 z-50 bg-opacity-40 flex items-center justify-center">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
-  );
-  
+  )
